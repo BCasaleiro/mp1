f19a60ae99de91c64f703cfc268bf991b27dd263
Bernardo Casaleiro <bernardocasaleiro@gmail.com>
2016-10-11 19:07:17 +0100

    added compilation and drawing of the lh -> 2, nh -> 3, rr -> 4 and ss -> s transducers to the script

--- run.sh
+++ run.sh
@@ -28,3 +28,27 @@ rm -f output/2nd/ch-x.fst output/2nd/ch-x.pdf
 
 fstcompile --isymbols=letras.sym --osymbols=letras.sym  src/2nd/ch-x.txt | fstarcsort > output/2nd/ch-x.fst
 fstdraw    --isymbols=letras.sym --osymbols=letras.sym  output/2nd/ch-x.fst | dot -Tpdf  > output/2nd/ch-x.pdf
+
+# lh -> 2
+rm -f output/2nd/lh-2.fst output/2nd/lh-2.pdf
+
+fstcompile --isymbols=letras.sym --osymbols=letras.sym  src/2nd/lh-2.txt | fstarcsort > output/2nd/lh-2.fst
+fstdraw    --isymbols=letras.sym --osymbols=letras.sym  output/2nd/lh-2.fst | dot -Tpdf  > output/2nd/lh-2.pdf
+
+# nh -> 3
+rm -f output/2nd/nh-3.fst output/2nd/nh-3.pdf
+
+fstcompile --isymbols=letras.sym --osymbols=letras.sym  src/2nd/nh-3.txt | fstarcsort > output/2nd/nh-3.fst
+fstdraw    --isymbols=letras.sym --osymbols=letras.sym  output/2nd/nh-3.fst | dot -Tpdf  > output/2nd/nh-3.pdf
+
+# rr -> 4
+rm -f output/2nd/rr-4.fst output/2nd/rr-4.pdf
+
+fstcompile --isymbols=letras.sym --osymbols=letras.sym  src/2nd/rr-4.txt | fstarcsort > output/2nd/rr-4.fst
+fstdraw    --isymbols=letras.sym --osymbols=letras.sym  output/2nd/rr-4.fst | dot -Tpdf  > output/2nd/rr-4.pdf
+
+# ss -> s
+rm -f output/2nd/ss-s.fst output/2nd/ss-s.pdf
+
+fstcompile --isymbols=letras.sym --osymbols=letras.sym  src/2nd/ss-s.txt | fstarcsort > output/2nd/ss-s.fst
+fstdraw    --isymbols=letras.sym --osymbols=letras.sym  output/2nd/ss-s.fst | dot -Tpdf  > output/2nd/ss-s.pdf
