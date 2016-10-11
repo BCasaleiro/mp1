153045599c2ed9959a76244939b79093b2021f15
Joao Godinho <joaogodinho_4@hotmail.com>
2016-10-11 22:20:20 +0100

    added all tests necessary

--- run.sh
+++ run.sh
@@ -54,20 +54,29 @@ fstcompile --isymbols=letras.sym --osymbols=letras.sym  src/1st/x_s.txt | fstarc
 fstdraw    --isymbols=letras.sym --osymbols=letras.sym  output/1st/x_s.fst | dot -Tpdf  > output/1st/x_s.pdf
 
 #Combine the three transductors:
-fstunion output/1st/x_s.fst output/1st/x_z.fst > output/1st/step_t.fst
+fstunion output/1st/s_z.fst output/1st/x_s.fst > output/1st/step_t1.fst
+fstunion output/1st/step_t1.fst output/1st/x_z.fst > output/1st/step_t2.fst
 
-fstrmepsilon output/1st/step_t.fst > output/1st/step1.fst
+fstrmepsilon output/1st/step_t2.fst > output/1st/step1.fst
 
-fstdraw    --isymbols=letras.sym --osymbols=letras.sym  output/1st/step1.fst | dot -Tpdf  > output/1st/step2.pdf
+fstdraw    --isymbols=letras.sym --osymbols=letras.sym  output/1st/step1.fst | dot -Tpdf  > output/1st/step1.pdf
 
 #testes:
-#exame
-fstcompose output/1st/exame.fst output/1st/step1.fst > output/1st/test1.fst
+#casa
+fstcompose output/1st/casa.fst output/1st/step1.fst > output/1st/test1.fst
 fstdraw --isymbols=letras.sym --osymbols=letras.sym  output/1st/test1.fst | dot -Tpdf > output/1st/test1.pdf
 
-#extra
-fstcompose output/1st/extra.fst output/1st/step1.fst > output/1st/test2.fst
+#exame
+fstcompose output/1st/exame.fst output/1st/step1.fst > output/1st/test2.fst
 fstdraw --isymbols=letras.sym --osymbols=letras.sym  output/1st/test2.fst | dot -Tpdf > output/1st/test2.pdf
+
+#exame
+fstcompose output/1st/exame.fst output/1st/step1.fst > output/1st/test3.fst
+fstdraw --isymbols=letras.sym --osymbols=letras.sym  output/1st/test3.fst | dot -Tpdf > output/1st/test3.pdf
+
+#extra
+fstcompose output/1st/extra.fst output/1st/step1.fst > output/1st/test4.fst
+fstdraw --isymbols=letras.sym --osymbols=letras.sym  output/1st/test4.fst | dot -Tpdf > output/1st/test4.pdf
 
 
 #fstcompose output/1st/asa.fst output/1st/step1.fst > output/1st/test2.fst
