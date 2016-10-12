8c02a309d84307b52ec144e86369f63af335cb5b
Bernardo Casaleiro <bernardocasaleiro@gmail.com>
2016-10-12 20:15:18 +0100

    some changes

--- run.sh
+++ run.sh
@@ -4,22 +4,6 @@
 # global
 mkdir -p output
 
-# 1st
-# mkdir -p output/1st
-#
-# rm -f output/1st/s_z.fst output/1st/s_z.pdf
-# rm -f output/1st/x_z.fst output/1st/x_z.pdf
-# rm -f output/1st/x_s.fst outputs/1st/x_s.pdf
-#
-# fstcompile --isymbols=letras.sym --osymbols=letras.sym  src/1st/s_z.txt | fstarcsort > outputs/1st/s_z.fst
-# fstdraw    --isymbols=letras.sym --osymbols=letras.sym  output/1st/s_z.fst | dot -Tpdf  > output/1st/s_z.pdf
-#
-# fstcompile --isymbols=letras.sym --osymbols=letras.sym  src/1st/x_z.txt | fstarcsort > output/1st/x_z.fst
-# fstdraw    --isymbols=letras.sym --osymbols=letras.sym  output/1st/x_z.fst | dot -Tpdf  > output/1st/x_z.pdf
-#
-# fstcompile --isymbols=letras.sym --osymbols=letras.sym  src/1st/x_s.txt | fstarcsort > output/1st/x_s.fst
-# fstdraw    --isymbols=letras.sym --osymbols=letras.sym  output/1st/x_s.fst | dot -Tpdf  > output/1st/x_s.pdf
-
 # 2nd
 mkdir -p output/2nd
 
@@ -67,5 +51,5 @@ rm -f output/2nd/rr-4.fst output/2nd/rr-4.pdf
 #-------------------------------------------------------------------------------
 rm -f output/2nd/ss-s.fst output/2nd/ss-s.pdf
 
-fstcompile --isymbols=letras.sym --osymbols=letras.sym  src/2nd/ss-s.txt | fstarcsort > output/2nd/ss-s.fst
-fstdraw    --isymbols=letras.sym --osymbols=letras.sym  output/2nd/ss-s.fst | dot -Tpdf  > output/2nd/ss-s.pdf
+#fstcompile --isymbols=letras.sym --osymbols=letras.sym  src/2nd/ss-s.txt | fstarcsort > output/2nd/ss-s.fst
+#fstdraw    --isymbols=letras.sym --osymbols=letras.sym  output/2nd/ss-s.fst | dot -Tpdf  > output/2nd/ss-s.pdf
