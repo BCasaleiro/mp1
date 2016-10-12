a855636b6ddeae4e0d80e2b4c4488416daf95970
Bernardo Casaleiro <bernardocasaleiro@gmail.com>
2016-10-12 22:52:14 +0100

    added all the tests for the ch -> x transducer to the script

--- run.sh
+++ run.sh
@@ -15,13 +15,33 @@ fstcompile --isymbols=letras.sym --osymbols=letras.sym  src/2nd/ch-x.txt | fstar
 fstdraw    --isymbols=letras.sym --osymbols=letras.sym  output/2nd/ch-x.fst | dot -Tpdf  > output/2nd/ch-x.pdf
 
 # test cases
-rm -f output/2nd/r-chucha.pdf
+rm -f output/2nd/r-*.pdf
 
 fstcompile --isymbols=letras.sym --osymbols=letras.sym  input/2nd/ch-x/chucha.txt | fstarcsort > output/2nd/chucha.fst
 fstcompose output/2nd/chucha.fst output/2nd/ch-x.fst > output/2nd/r-chucha.fst
 fstdraw --isymbols=letras.sym --osymbols=letras.sym  output/2nd/r-chucha.fst | dot -Tpdf > output/2nd/r-chucha.pdf
 
-rm -r output/2nd/chucha.fst output/2nd/r-chucha.fst
+fstcompile --isymbols=letras.sym --osymbols=letras.sym  input/2nd/ch-x/acachar.txt | fstarcsort > output/2nd/acachar.fst
+fstcompose output/2nd/acachar.fst output/2nd/ch-x.fst > output/2nd/r-acachar.fst
+fstdraw --isymbols=letras.sym --osymbols=letras.sym  output/2nd/r-acachar.fst | dot -Tpdf > output/2nd/r-acachar.pdf
+
+fstcompile --isymbols=letras.sym --osymbols=letras.sym  input/2nd/ch-x/achacar.txt | fstarcsort > output/2nd/achacar.fst
+fstcompose output/2nd/achacar.fst output/2nd/ch-x.fst > output/2nd/r-achacar.fst
+fstdraw --isymbols=letras.sym --osymbols=letras.sym  output/2nd/r-achacar.fst | dot -Tpdf > output/2nd/r-achacar.pdf
+
+fstcompile --isymbols=letras.sym --osymbols=letras.sym  input/2nd/ch-x/acho.txt | fstarcsort > output/2nd/acho.fst
+fstcompose output/2nd/acho.fst output/2nd/ch-x.fst > output/2nd/r-acho.fst
+fstdraw --isymbols=letras.sym --osymbols=letras.sym  output/2nd/r-acho.fst | dot -Tpdf > output/2nd/r-acho.pdf
+
+fstcompile --isymbols=letras.sym --osymbols=letras.sym  input/2nd/ch-x/chines.txt | fstarcsort > output/2nd/chines.fst
+fstcompose output/2nd/chines.fst output/2nd/ch-x.fst > output/2nd/r-chines.fst
+fstdraw --isymbols=letras.sym --osymbols=letras.sym  output/2nd/r-chines.fst | dot -Tpdf > output/2nd/r-chines.pdf
+
+fstcompile --isymbols=letras.sym --osymbols=letras.sym  input/2nd/ch-x/pichar.txt | fstarcsort > output/2nd/pichar.fst
+fstcompose output/2nd/pichar.fst output/2nd/ch-x.fst > output/2nd/r-pichar.fst
+fstdraw --isymbols=letras.sym --osymbols=letras.sym  output/2nd/r-pichar.fst | dot -Tpdf > output/2nd/r-pichar.pdf
+
+rm -r output/2nd/*.fst
 
 # lh -> 2
 #-------------------------------------------------------------------------------
