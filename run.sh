a3825a460a34ea8b4f9cd14fbc3d067a6871d62c
Joao Godinho <joaogodinho_4@hotmail.com>
2016-10-12 01:35:00 +0100

    added rules for test cases

--- run.sh
+++ run.sh
@@ -24,7 +24,6 @@ rm -f output/1st/casa.fst output/1st/casa.pdf
 rm -f output/1st/asa.fst output/1st/asa.pdf
 rm -f output/1st/exame.fst output/1st/exame.pdf
 rm -f output/1st/extra.fst output/1st/extra.pdf
-
 rm -f output/1st/test1.fst output/1st/test1.pdf
 rm -f output/1st/test2.fst output/1st/test2.pdf
 rm -f output/1st/test3.fst output/1st/test3.pdf
@@ -85,3 +84,50 @@ fstdraw --isymbols=letras.sym --osymbols=letras.sym  output/1st/test4.fst | dot 
 
 
 #2nd
+
+
+
+#3rd
+
+mkdir -p output/3rd
+
+ 	#transdutores
+
+	#temporary
+
+	#testes
+rm -f output/3rd/quanto.fst output/3rd/quanto.pdf
+rm -f output/3rd/celofane.fst output/3rd/celofane.pdf
+rm -f output/3rd/calar.fst output/3rd/calar.pdf
+rm -f output/3rd/paz.fst output/3rd/paz.pdf
+rm -f output/3rd/gelo.fst output/3rd/gelo.pdf
+rm -f output/3rd/galo.fst output/3rd/galo.pdf
+rm -f output/3rd/xerox.fst output/3rd/xerox.pdf
+
+	#teste "quanto"
+fstcompile --isymbols=letras.sym --osymbols=letras.sym  input/3rd/quanto.txt | fstarcsort > output/3rd/quanto.fst
+fstdraw    --isymbols=letras.sym --osymbols=letras.sym  output/3rd/quanto.fst | dot -Tpdf  > output/3rd/quanto.pdf
+
+	#teste "celofane"
+fstcompile --isymbols=letras.sym --osymbols=letras.sym  input/3rd/celofane.txt | fstarcsort > output/3rd/celofane.fst
+fstdraw    --isymbols=letras.sym --osymbols=letras.sym  output/3rd/celofane.fst | dot -Tpdf  > output/3rd/celofane.pdf
+	
+	#teste "calar"
+fstcompile --isymbols=letras.sym --osymbols=letras.sym  input/3rd/calar.txt | fstarcsort > output/3rd/calar.fst
+fstdraw    --isymbols=letras.sym --osymbols=letras.sym  output/3rd/calar.fst | dot -Tpdf  > output/3rd/calar.pdf
+
+	#teste "paz"
+fstcompile --isymbols=letras.sym --osymbols=letras.sym  input/3rd/paz.txt | fstarcsort > output/3rd/paz.fst
+fstdraw    --isymbols=letras.sym --osymbols=letras.sym  output/3rd/paz.fst | dot -Tpdf  > output/3rd/paz.pdf
+
+#teste "gelo"
+fstcompile --isymbols=letras.sym --osymbols=letras.sym  input/3rd/gelo.txt | fstarcsort > output/3rd/gelo.fst
+fstdraw    --isymbols=letras.sym --osymbols=letras.sym  output/3rd/gelo.fst | dot -Tpdf  > output/3rd/gelo.pdf
+
+#teste "galo"
+fstcompile --isymbols=letras.sym --osymbols=letras.sym  input/3rd/galo.txt | fstarcsort > output/3rd/galo.fst
+fstdraw    --isymbols=letras.sym --osymbols=letras.sym  output/3rd/galo.fst | dot -Tpdf  > output/3rd/galo.pdf
+
+#teste "xerox"
+fstcompile --isymbols=letras.sym --osymbols=letras.sym  input/3rd/xerox.txt | fstarcsort > output/3rd/xerox.fst
+fstdraw    --isymbols=letras.sym --osymbols=letras.sym  output/3rd/xerox.fst | dot -Tpdf  > output/3rd/xerox.pdf
