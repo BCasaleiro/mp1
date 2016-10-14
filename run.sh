d45b03c1cc3d09c89ce89005f0cb44c396535404
Bernardo Casaleiro <bernardocasaleiro@gmail.com>
2016-10-14 14:59:15 +0100

    fix small bug with the composition

--- run.sh
+++ run.sh
@@ -51,7 +51,7 @@ fstcompose output/1st/1st.fst output/2nd/2nd.fst > output/final/t-1.fst
 # fstdraw --isymbols=syms.txt --osymbols=syms.txt  output/final/t-1.fst | dot -Tpdf > output/final/t-1.pdf
 # # t-1 with 3rd -> t-2
 # #-------------------------------------------------------------------------------
-fstcompose output/final/t-1.fst output/4th/4th.fst > output/final/t-2.fst
+fstcompose output/final/t-1.fst output/3rd/3rd.fst > output/final/t-2.fst
 # fstdraw --isymbols=syms.txt --osymbols=syms.txt  output/final/t-2.fst | dot -Tpdf > output/final/t-2.pdf
 # # t-2 with 4th -> final
 # #-------------------------------------------------------------------------------
