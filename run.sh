1763c3812d17db9b65396ccafc9a84813d1a1589
Bernardo Casaleiro <bernardocasaleiro@gmail.com>
2016-10-14 15:21:00 +0100

    reversing totally

--- run.sh
+++ run.sh
@@ -51,11 +51,11 @@ fstcompose output/1st/1st.fst output/2nd/2nd.fst > output/final/t-1.fst
 # fstdraw --isymbols=syms.txt --osymbols=syms.txt  output/final/t-1.fst | dot -Tpdf > output/final/t-1.pdf
 # # t-1 with 3rd -> t-2
 # #-------------------------------------------------------------------------------
-fstcompose output/3rd/3rd.fst output/final/t-1.fst > output/final/t-2.fst
+fstcompose output/final/t-1.fst output/3rd/3rd.fst > output/final/t-2.fst
 # fstdraw --isymbols=syms.txt --osymbols=syms.txt  output/final/t-2.fst | dot -Tpdf > output/final/t-2.pdf
 # # t-2 with 4th -> final
 # #-------------------------------------------------------------------------------
-fstcompose output/4th/4th.fst output/final/t-2.fst > output/final/final.fst
+fstcompose output/final/t-2.fst output/4th/4th.fst > output/final/final.fst
 # fstdraw --isymbols=syms.txt --osymbols=syms.txt  output/final/final.fst | dot -Tpdf > output/final/final.pdf
 
 #-------------------------------------------------------------------------------
